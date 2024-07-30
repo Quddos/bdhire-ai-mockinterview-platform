3220b959322e89ca310ab43a5d194ddda42fa56c
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2024-07-30 17:32:23 +0530

    reccord answer interface  completed

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,11 +1,13 @@
 
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 import Webcam from 'react-webcam'
 
 function RecordAnswerSection() {
   return (
-    <div className='flex flex-col justify-center items-center my-20 bg-slate-400 rounded-lg p-5'>
+    <div className='flex items-center flex-col justify-center '>
+        <div className='flex flex-col justify-center items-center mt-20 bg-slate-400 rounded-lg p-5'>
         <Image src={'/webcam.png'} width={400} height={200}
         className='absolute'/>
         <Webcam
@@ -15,10 +17,12 @@ function RecordAnswerSection() {
             width: '100%',
             zIndex:10,
             
-            
+            // bdhire-ai-mockinterview-platform
          
 
         }}/>
+    </div>
+    <Button variant="outline" className="my-10">Record Answer</Button>
     </div>
   )
 }
