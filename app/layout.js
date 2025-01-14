8b09cd25f28dd963d3695f2031df6b9e1ae4c0cd
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2025-01-14 08:44:53 +0530

    bdhire to qudmeet.click

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,8 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "BdHire AI",
-  description: "Job and Interview Prep",
+  title: "Qudmeet.click",
+  description: "Qudmeet is SaaS AI automation and Career Acing application, establish to help you build, earn, and enhance neccessary technologies required to land your dreama and aspiration. We provide varies of Tools and Features such as AI Demo interview, Job Opportunity, File Conversion and tools etc.",
 };
 
 export default function RootLayout({ children }) {
